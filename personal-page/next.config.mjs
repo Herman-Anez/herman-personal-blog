6487d8c8305e76dce2ac8a724640b7111bc9c7d0
herman <herman.anez.dev@gmail.com>
2026-05-19 21:31:59 +0200

    .

--- personal-page/next.config.mjs
+++ personal-page/next.config.mjs
@@ -9,6 +9,7 @@ const withMDX = mdx({
 const nextConfig = {
   // Habilita la compilación estática nativa (HTML/CSS/JS puros) apta para CDNs como GitHub Pages
   output: "export",
+  basePath: "/herman-personal-blog",
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   transpilePackages: ["next-mdx-remote"],
   images: {
